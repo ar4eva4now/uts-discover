074ee291cb498714bcf53c94dd97e70826781afb
jacobvorr <jacobvorreiter@gmail.com>
2019-04-12 15:03:27 +1000

    Screen Overlay Help Button with world space buttons. First prototype of buttons facing camera

---
+++ Assets/Scripts/ButtonsFaceCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsFaceCamera : MonoBehaviour
+{
+    public Camera main_camera;
+
+    void Update()
+    {
+        Vector3 v = main_camera.transform.position - transform.position;
+
+        v.x = v.z = 0.0f;
+        transform.LookAt(main_camera.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
