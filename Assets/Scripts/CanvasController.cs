ed39de211228b68d1e63bf486d952dc38fd67464
Jonathan Moallem <jdmoallem@gmail.com>
2019-04-17 02:03:55 +1000

    Renamed and updated canvas controller

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/**
+ * A class to manage the button canvas.
+ */
+public class CanvasController : MonoBehaviour
+{
+    // GameObject fields
+    public Camera mainCamera;
+
+    private void Update()
+    {
+        // Make the buttons face the camera
+        var cameraPos = mainCamera.transform.position;
+        var v = cameraPos - transform.position;
+
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraPos - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
