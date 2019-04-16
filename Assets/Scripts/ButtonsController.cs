3a158d8e50aa8f1728d71543fc1c4e65080a6bad
Jonathan Moallem <jdmoallem@gmail.com>
2019-04-17 02:04:06 +1000

    Added buttons controller

---
+++ Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsController : MonoBehaviour
+{
+    // GameObject fields
+    public Button welcomeButton;
+    public Button locationButton;
+    public Button innovationButton;
+    public Button statsButton;
+    
+    private void Start()
+    {
+        welcomeButton.onClick.AddListener(TaskOnClick);
+    }
+
+    
+    private static void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+}
