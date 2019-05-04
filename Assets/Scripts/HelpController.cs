fc6bb025fd837bedb7d693e817af4589a7a7f55e
Valentina A <valentinaa632@gmail.com>
2019-05-04 20:42:33 +1000

    full screen panel for all resolutions

--- Assets/Scripts/HelpController.cs
+++ Assets/Scripts/HelpController.cs
@@ -8,9 +8,20 @@ public class HelpController : MonoBehaviour
 {
     // GameObject fields
     public Button helpButton;
-    
+    public Canvas parent;
+
     // Symbol fields
     private static readonly int SlideIn = Animator.StringToHash("SlideIn");
+
+    /**
+     * Adjust the size of the panel to suit the canvas size.
+     */
+    private void Update()
+    {
+        Rect rootCanvasRect = parent.GetComponent<RectTransform>().rect; //Gets the Sizing of the RectTransform
+        RectTransform panelRect = gameObject.GetComponent<RectTransform>(); // Gets the RectTransform (to manipulate the size of the panel);
+        panelRect.sizeDelta = new Vector2(rootCanvasRect.width, rootCanvasRect.height); // Adjust the size of the panel
+    }
 
     /**
      * A public function to toggle the gameObject's display and update
@@ -18,18 +29,49 @@ public class HelpController : MonoBehaviour
      */
     public void ToggleOpen()
     {
-        var animator = gameObject.GetComponent<Animator>();
-        
+
+        //Use proper datatypes for better memory management rather than vars. 
+        Animator animator = gameObject.GetComponent<Animator>();
+
         // Play the slide in animation if the component is accessible
         if (animator == null) return;
-        var isSlideIn = animator.GetBool(SlideIn);
+
+        //Use proper datatypes for better memory management rather than vars.
+        bool isSlideIn = animator.GetBool(SlideIn);
         animator.SetBool(SlideIn, !isSlideIn);
-            
+
         // Load the appropriate button sprite
-        var filePath = isSlideIn ? "uts-discover-ui-help" : "uts-discover-ui-exit";
-        var sp  = Resources.Load<Sprite>(filePath);
-            
+        string filePath = isSlideIn ? "uts-discover-ui-help" : "uts-discover-ui-exit";
+        Sprite sp  = Resources.Load<Sprite>(filePath);
+
         // Apply the button sprite
         helpButton.GetComponent<Image>().sprite = sp;
     }
+
+    //// GameObject fields
+    //public Button helpButton;
+
+    //// Symbol fields
+    //private static readonly int SlideIn = Animator.StringToHash("SlideIn");
+
+    ///**
+    // * A public function to toggle the gameObject's display and update
+    // * the UI appropriately.
+    // */
+    //public void ToggleOpen()
+    //{
+    //    var animator = gameObject.GetComponent<Animator>();
+
+    //    // Play the slide in animation if the component is accessible
+    //    if (animator == null) return;
+    //    var isSlideIn = animator.GetBool(SlideIn);
+    //    animator.SetBool(SlideIn, !isSlideIn);
+
+    //    // Load the appropriate button sprite
+    //    var filePath = isSlideIn ? "uts-discover-ui-help" : "uts-discover-ui-exit";
+    //    var sp  = Resources.Load<Sprite>(filePath);
+
+    //    // Apply the button sprite
+    //    helpButton.GetComponent<Image>().sprite = sp;
+    //}
 }
