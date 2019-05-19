a3f14e08e44c2a9d4ef47b83f1bd569de7d2aebd
Jonathan Moallem <jdmoallem@gmail.com>
2019-05-19 14:24:46 +1000

    Added a scene loader script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+}
