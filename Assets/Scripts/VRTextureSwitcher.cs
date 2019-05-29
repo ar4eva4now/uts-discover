bdf32e4cde70ebc497cbde5c6f0fe5dd5f8375a8
Jonathan Moallem <jdmoallem@gmail.com>
2019-05-29 13:53:48 +1000

    Added texture switcher script

---
+++ Assets/Scripts/VRTextureSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTextureSwitcher : MonoBehaviour
+{
+    public Material newMaterial;
+
+    public void SwitchMaterial()
+    {
+        var mats = GetComponent<Renderer>().materials;
+        mats[0] = newMaterial;
+        GetComponent<Renderer>().materials = mats;
+    }
+}
