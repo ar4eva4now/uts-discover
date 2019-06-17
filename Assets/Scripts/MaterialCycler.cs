6eb432dc1567fb7c8d461a70747eac16956f807b
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-17 12:54:55 +1000

    Added material cycler script

---
+++ Assets/Scripts/MaterialCycler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MaterialSwitcher : MonoBehaviour
+{
+    public void SwitchMaterial(Material newMaterial)
+    {
+        var mats = this.GetComponent<Renderer>().materials;
+        mats[0] = newMaterial;
+        this.GetComponent<Renderer>().materials = mats;
+    }
+}
