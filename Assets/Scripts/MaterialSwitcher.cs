3b61399fd2ec2842928eebe141fdbd48e76a7e7e
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-17 13:14:54 +1000

    Commented and cleaned up material switcher

--- Assets/Scripts/MaterialSwitcher.cs
+++ Assets/Scripts/MaterialSwitcher.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 
+/**
+ * A class to help switch GameObject materials on runtime.
+ */
 public class MaterialSwitcher : MonoBehaviour
 {
+    /**
+     * A public function to switch the GameObject's 0th material property
+     */
     public void SwitchMaterial(Material newMaterial)
     {
-        var mats = this.GetComponent<Renderer>().materials;
+        // Change the first (and only) material over to the one given
+        var mats = GetComponent<Renderer>().materials;
         mats[0] = newMaterial;
-        this.GetComponent<Renderer>().materials = mats;
+        GetComponent<Renderer>().materials = mats;
     }
 }
