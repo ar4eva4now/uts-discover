6118ff88a72eb1999b8db8b149609cd2275e697f
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-17 13:15:36 +1000

    Made material cycler script cycle through the list of cyclable object

--- Assets/Scripts/MaterialCycler.cs
+++ Assets/Scripts/MaterialCycler.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
 
+/**
+ * A class to help cycle through a list of materials at runtime.
+ * To be used in conjunction with the MaterialSwitcher.
+ */
 public class MaterialCycler : MonoBehaviour
 {
+    // Public fields
     public Material[] materials;
     
+    // Private fields
+    private int _current = 0;
+    
+    /**
+     * A public function to cycle to the next material.
+     */
     public void CycleMaterial()
     {
-        var mats = this.GetComponent<Renderer>().materials;
-        mats[0] = materials[0];
-        this.GetComponent<Renderer>().materials = mats;
+        // Get a copy of the MaterialSwitcher and make the switch
+        var materialSwitcher = GetComponent<MaterialSwitcher>();
+        materialSwitcher.SwitchMaterial(materials[_current]);
+        
+        // Increment the value, clamping it between 0 and 2
+        _current = ++_current % 3;
     }
 }
